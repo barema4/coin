a9922e7c72f4f84fe6e365ac6ec40b374099e703
sam rubarema <samrubarema6@gmail.com>
2023-07-22 15:06:36 +0300

    modifed the chart and styles

--- src/components/CryptoTrackerChart.jsx
+++ src/components/CryptoTrackerChart.jsx
@@ -1,16 +1,16 @@
-import { LineChart, Line, CartesianGrid, Tooltip, Legend } from "recharts";
+import { LineChart, Line, Tooltip, Legend } from "recharts";
 import "../assets/style.css"
 const CryptoTrackerChart = ({ chartData }) => {
   return (
     <div className="crypto-chart-container">
       <LineChart width={300} height={200} data={chartData}>
-        <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
         <Legend />
         <Line
           type="monotone"
+          fill="rgb(255, 247, 238)"
           dataKey="price"
-          stroke="#007BFF"
+          stroke="hsl(24.05deg 100% 68%)"
           name="1 BTC=$30,389"
         />
       </LineChart>
@@ -19,3 +19,4 @@ const CryptoTrackerChart = ({ chartData }) => {
 };
 
 export default CryptoTrackerChart;
+// hsl(24.05deg 100% 68%)
